24420d4ac54a9c8d8068ea7029aa474b2494d1b0
V <84370300+vian-yayaya@users.noreply.github.com>
2026-02-06 21:02:49 +0000

    Update: moved config files and added jupyter

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -8,6 +8,7 @@ print('arguments',sys.argv)
 month = int(sys.argv[1])
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df['month']=month
 print(df.head())
 
 df.to_parquet(f"output_{month}.parquet")
